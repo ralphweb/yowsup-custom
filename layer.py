868e310e285a0b80bfbee0c399a565488bc23fe7
Rodolfo Inostroza <ralphweb@gmail.com>
2017-07-01 23:36:23 -0400

    primer commit

---
+++ layer.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
+from yowsup.layers.protocol_media.protocolentities     import DownloadableMediaMessageProtocolEntity
+from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
+from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
+from pymongo import MongoClient
+import datetime
+
+
+class EchoLayer(YowInterfaceLayer):
+    global client 
+    client = MongoClient('mongodb://localhost:27017/')
+    global db 
+    db = client.whatsapp
+    global whatsapp 
+    whatsapp = db.messages
+    global topics 
+    topics = db.topics
+
+    global topiclist
+    topiclist = []
+    global invitados
+    invitados = []
+
+    for topic in topics.find():
+        if topic["active"]:
+            invitados.append(topic["hash"])
+            for keyword in topic["keywords"]:
+                topiclist.append([topic["hash"],keyword])
+
+    @ProtocolEntityCallback("message")
+    def onMessage(self, messageProtocolEntity):
+        #send receipt otherwise we keep receiving the same message over and over
+        
+        if True:
+            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
+            
+            if messageProtocolEntity.getType()=='text':
+                number = messageProtocolEntity.getFrom().split('@')[0]
+                if messageProtocolEntity.getBody().find('#')!=-1:
+                    #Contiene hashtag, puede ser voto
+                    isVote = False
+                    topicFound = ""
+                    cant = 0
+                    for topic in topiclist:
+                         if messageProtocolEntity.getBody().lower().find(topic[1].lower())!=-1:
+                            isVote = True
+                            cant += 1
+                            topicFound = topic[0]
+                    if isVote:  
+                        if cant>1:
+                            #Tiene mas de un voto a la vez
+                            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                                "Sólo puedes votar por 1 participante a la vez. Por favor elige uno y vuelve a intentar",
+                                to = messageProtocolEntity.getFrom())
+                            self.toLower(outgoingMessageProtocolEntity)
+                            msg = {"name": messageProtocolEntity.getNotify(),"number": number,"text": messageProtocolEntity.getBody(),"vote":False,"date": datetime.datetime.utcnow()}
+                            whatsapp.insert_one(msg)  
+                        else:
+                            votes = whatsapp.find_one({"$and":[{"number": number},{"vote":True}]})
+                            if votes==None:
+                                outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                                    "Hemos recibido tu voto. Gracias por participar 👍🏻",
+                                    to = messageProtocolEntity.getFrom())
+                                msg = {"name": messageProtocolEntity.getNotify(),"number": number,"text": messageProtocolEntity.getBody(),"vote":True,"topic":topicFound,"date": datetime.datetime.utcnow()}
+                                whatsapp.insert_one(msg)
+                            else:
+                                outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                                    "Sólo puedes votar 1 vez. Por favor espera la próxima votación. Tu último voto fue por "+votes["topic"].encode('utf8'),
+                                    to = messageProtocolEntity.getFrom())
+                                msg = {"name": messageProtocolEntity.getNotify(),"number": number,"text": messageProtocolEntity.getBody(),"vote":False,"date": datetime.datetime.utcnow()}
+                                whatsapp.insert_one(msg)
+                            self.toLower(outgoingMessageProtocolEntity)
+                    else:        
+                        invitadoslist = ', '.join(invitados)
+                        outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                            "Recuerda votar por uno de los invitados: "+invitadoslist,
+                            to = messageProtocolEntity.getFrom())
+                        self.toLower(outgoingMessageProtocolEntity)
+                        msg = {"name": messageProtocolEntity.getNotify(),"number": number,"text": messageProtocolEntity.getBody(),"vote":False,"date": datetime.datetime.utcnow()}
+                        whatsapp.insert_one(msg)
+                else:
+                    msg = {"name": messageProtocolEntity.getNotify(),"number": number,"text": messageProtocolEntity.getBody(),"vote":False,"date": datetime.datetime.utcnow()}
+                    whatsapp.insert_one(msg)                
+                
+                print messageProtocolEntity.getFrom(),messageProtocolEntity.getBody()
+            elif messageProtocolEntity.getType()=='media':
+                outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                    "Lo siento, no estamos recibiendo archivos",
+                    to = messageProtocolEntity.getFrom())
+                self.toLower(outgoingMessageProtocolEntity)
+                print 'media message'
+            
+            self.toLower(receipt)
+
+    @ProtocolEntityCallback("receipt")
+    def onReceipt(self, entity):
+        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
+        self.toLower(ack)
---
+++ run.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+from yowsup.stacks                             import YowStackBuilder
+from yowsup.common                             import YowConstants
+from yowsup.layers                             import YowLayerEvent
+from layer                                     import EchoLayer
+from yowsup.layers.auth                        import YowAuthenticationProtocolLayer
+from yowsup.layers.coder                       import YowCoderLayer
+from yowsup.layers.network                     import YowNetworkLayer
+from yowsup.env                                import YowsupEnv
+
+#Uncomment to log
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+CREDENTIALS = ("56996432074", "KjwqK+LIyJvL8DiVnV8PDfxVkDk=") #replace with your phone and password
+
+if __name__==  "__main__":
+    stackBuilder = YowStackBuilder()
+
+    stack = stackBuilder\
+        .pushDefaultLayers(True)\
+        .push(EchoLayer)\
+        .build()
+
+    stack.setProp(YowAuthenticationProtocolLayer.PROP_CREDENTIALS, CREDENTIALS)       #setting credentials
+    stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))          #sending the connect signal
+    stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])           #whatsapp server address
+    stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)
+    stack.setProp(YowCoderLayer.PROP_RESOURCE, YowsupEnv.getCurrent().getResource())  #info about us as WhatsApp client
+
+    stack.loop( timeout = 0.5, discrete = 0.5 )
